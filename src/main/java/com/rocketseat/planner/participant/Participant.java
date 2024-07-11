732fdc3d59268b0e6adf65164b6f812c66332b45
RicardoBarrote <ricardoambscfc@gmail.com>
2024-07-11 19:42:50 -0300

    Participating entity created
    Signed-off-by: RicardoBarrote <ricardoambscfc@gmail.com>

---
+++ src/main/java/com/rocketseat/planner/participant/Participant.java
@@ -0,0 +1,70 @@
+package com.rocketseat.planner.participant;
+
+import com.rocketseat.planner.trip.Trip;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "participants")
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+    public Participant () {
+    }
+
+    public Participant(UUID id, String name, String email, Boolean isConfirmed) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.isConfirmed = isConfirmed;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getConfirmed() {
+        return isConfirmed;
+    }
+
+    public void setConfirmed(Boolean confirmed) {
+        isConfirmed = confirmed;
+    }
+}
