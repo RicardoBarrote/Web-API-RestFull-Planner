9b345417a9eb8807b174c15ce6dc9a5fef5712fc
RicardoBarrote <ricardoambscfc@gmail.com>
2024-07-11 18:46:47 -0300

    End point update, trip entity
    Signed-off-by: RicardoBarrote <ricardoambscfc@gmail.com>

--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -25,7 +27,8 @@ public class TripController {
     this.tripRepository.save(newTrip);
     this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
     return ResponseEntity.ok().body(new TripCreateResponse(newTrip.getId()));
-};
+
+   };
 
 
     //Explicando a expressão Lambda abaixo.
@@ -36,12 +39,28 @@ public class TripController {
 
     /*Caso a resposta for nulla, não exista um id correspondente,
      * ele monta uma ResponseEntity com o estado .notFound().build()*/
-
     @GetMapping("/{id}")
     public ResponseEntity<Trip> getTripDetails (@PathVariable UUID id){
-
         Optional<Trip> trip = this.tripRepository.findById(id);
         return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    };
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripRequestPayLoad payLoad){
+       Optional<Trip> trip = this.tripRepository.findById(id);
+
+       if(trip.isPresent()) {
+           Trip rawTrip = trip.get();
+
+           rawTrip.setStartsAt(LocalDateTime.parse(payLoad.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+           rawTrip.setEndsAt(LocalDateTime.parse(payLoad.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+           rawTrip.setDestination(payLoad.destination());
+
+           this.tripRepository.save(rawTrip);
+           return ResponseEntity.ok().body(rawTrip);
+       }
+
+       return ResponseEntity.notFound().build();
     };
 
 }
